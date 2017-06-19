110475936530f9bbfdff434b8eb1f48f371e75fc
Kike <kike@areavisual.net>
2017-06-19 17:41:18 +0200

    Arreglo
    Texto.

--- NL-process.php
+++ NL-process.php
@@ -1,32 +1,33 @@
 <?php
 
-$emailmanager = 'suscripcion@name.com';
+	$emailmanager = 'suscripcion@name.com';
 
 
-$scriptUrl = 'name/NL-confirm.php';
+	$scriptUrl = 'name/NL-confirm.php';
 
-$urlok = 'NL-ok.html';
-$urlko = 'NL-ko.html';
+	$urlok = 'NL-ok.html';
+	$urlko = 'NL-ko.html';
 
-error_reporting(0);
+	error_reporting(0);
 
-$email = trim($_POST['email']);
+	$email = trim($_POST['email']);
 
-$body = "Gracias por la suscripción!
+	$body = "Gracias por la suscripción!
 
-Para confirmar solicitud, haz clic en el siguiente enlace o copialo en el navegador:
+	Para confirmar solicitud, haz clic en el siguiente enlace o copialo en el navegador:
 
-$scriptUrl?email=$email
+	$scriptUrl?email=$email
 
-Si no has solicitado esta suscripción olvida este mensaje.
-";
+	Si no has solicitado esta suscripción olvida este mensaje.
+	";
 
 
-$Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
-if ($Ok) {
-	mail($email,'Por favor confirma la suscripción.',$body,'From: '.$emailmanager);
-	Header("Location: $urlok");
-} else {
-	Header("Location: $urlko");
-}
+	$Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
+	if ($Ok) {
+		mail($email,'Por favor confirma la suscripción.',$body,'From: '.$emailmanager);
+		Header("Location: $urlok");
+	} else {
+		Header("Location: $urlko");
+	}
+	
 ?>
