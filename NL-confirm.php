584188f8fa260beb61c53c221950d39dc4f78893
Kike <kike@areavisual.net>
2017-08-06 11:49:06 +0200

    Arreglos en archivos para colocar en el servidor
    Envío mensaje para confirmar y confirmar correo electrónico para la
    suscripción.

--- NL-confirm.php
+++ NL-confirm.php
@@ -2,17 +2,19 @@
 
 $emailmanager = 'suscripcion@name.com';
 
-$urlok = 'confirm-ok.html';
-$urlko = 'confirm-ko.html';
+$urlok = 'http://www.dominio.com/confirm-ok.html';
+$urlko = 'http://www.dominio.com/confirm-ko.html';
 
 error_reporting(0);
 
 $email = trim($_GET['email']);
 $Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
 if ($Ok) {
-	mail($emailmanager,'Subscribe','','From: '.$email);
+	mail($emailmanager,'Nueva suscripción.','','From: '.$email);
+	echo $urlok;
 	Header("Location: $urlok");
 } else {
+	echo $urlko;	
 	Header("Location: $urlko");
 }
 ?>
--- NL-process.php
+++ NL-process.php
@@ -1,32 +1,32 @@
 <?php
 
-	$emailmanager = 'suscripcion@name.com';
-
-	$scriptUrl = 'name/NL-confirm.php';
-
-	$urlok = 'NL-ok.html';
-	$urlko = 'NL-ko.html';
-
-	error_reporting(0);
-
-	$email = trim($_POST['email']);
-
-	$body = "Gracias por la suscripción!
-
-	Para confirmar solicitud, haz clic en el siguiente enlace o copialo en el navegador:
-
-	$scriptUrl?email=$email
-
-	Si no has solicitado esta suscripción olvida este mensaje.
-	";
+$emailmanager = 'suscripcion@name.com';
 
 
-	$Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
-	if ($Ok) {
-		mail($email,'Por favor confirma la suscripción.',$body,'From: '.$emailmanager);
-		Header("Location: $urlok");
-	} else {
-		Header("Location: $urlko");
-	}
-	
+$scriptUrl = 'http://www.dominio.com/NL-confirm.php';
+
+$urlok = 'NL-ok.html';
+$urlko = 'NL-ko.html';
+
+error_reporting(0);
+
+$email = trim($_POST['email']);
+
+$body = "Gracias por la suscripción!
+
+Para confirmar solicitud, haz clic en el siguiente enlace o copialo en el navegador:
+
+$scriptUrl?email=$email
+
+Si no has solicitado esta suscripción olvida este mensaje.
+";
+
+
+$Ok = ereg("^([a-zA-Z0-9_\.-]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", $email);
+if ($Ok) {
+	mail($email,'Por favor confirma la suscripción.',$body,'From: '.$emailmanager);
+	Header("Location: $urlok");
+} else {
+	Header("Location: $urlko");
+}
 ?>
